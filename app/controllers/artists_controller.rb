7151694fb0c48de686de52023c55fea801867a73
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-03-01 14:38:31 -0500

    Solved

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,10 @@
 class ArtistsController < ApplicationController
   def index
-    @artist = Artist.all
+    @artists= Artist.all
   end
 
   def show
-    @artist = Artist.find(params[:id])
-    @album = @artist.albums
+    @artists = Artist.find(params[:id])
+    @albums = @artists.albums
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+artist = Artist.create(name: 'Octavio Mesa', image_url: 'https://lastfm.freetls.fastly.net/i/u/ar0/460ba1a999814da071c4d963d5db2520.jpg')
+
+song = Song.create(name: 'La pelea con el diablo', number: '1', preview_url: 'https://open.spotify.com/track/4X2VGfqz2FabK9wWwGRw3Q?si=577d67b0d3f145b9')
+
+album = Album.create(name: 'Historia Musical de Octavio Mesa', image_url: 'https://i.ytimg.com/vi/Ma78M6f7UHY/maxresdefault.jpg', released_at: '', artist_id: artist.id)
+
+album.songs << song
+artist.albums << album
